e5399317f4d0c3e9defd01107dd5b9c2935b2e49
Ivii95 <38653840+Ivii95@users.noreply.github.com>
2018-10-10 20:18:16 +0200

    10/10/2018
    Inicio del proyecto y modelo de los datos.

---
+++ src/main/java/models/Calculator.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author Iván
+ */
+public class Calculator {
+    int valor1;
+    int valor2;
+    int resultado;
+    char signo;
+
+    public int getValor1() {
+        return valor1;
+    }
+
+    public void setValor1(int valor1) {
+        this.valor1 = valor1;
+    }
+
+    public int getValor2() {
+        return valor2;
+    }
+
+    public void setValor2(int valor2) {
+        this.valor2 = valor2;
+    }
+
+    public int getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(int resultado) {
+        this.resultado = resultado;
+    }
+
+    public char getSigno() {
+        return signo;
+    }
+
+    public void setSigno(char signo) {
+        this.signo = signo;
+    }
+    
+}
